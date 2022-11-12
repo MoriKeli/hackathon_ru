c6c23c086d0c79906ba0b90ac6b25269f32a450d
Mori Keli <kelimbth@gmail.com>
2022-11-12 15:20:20 +0300

    created an index_view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
+import json
 
-# Create your views here.
+def index_view(request):
+    API_KEY = "7tjuWYGmbmh3VRFytATeoSQqeaZ1CS"
+    BASE_URL = "http://api.kmhfltest.health.go.ke/api/facilities/facilities/?format=json"
+
+    REQUEST_URL = f''
+    response =  requests.get(REQUEST_URL)
+    
+    print(response.json())
+
+    context = {}
+    return render(request, 'templates/index.html', context)
+
