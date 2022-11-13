a6ea8ad3e90d9b17a8d7699ee17a09fdde903a7a
Mori Keli <kelimbth@gmail.com>
2022-11-13 07:57:35 +0300

    changed the token

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 def index_view(request):
-    API_KEY = "7tjuWYGmbmh3VRFytATeoSQqeaZ1CS"
+    API_KEY = "yE2wzM3G6eSS5FyhXLzPLsWPihwMFc"
     url_route  = "http://api.kmhfltest.health.go.ke/api/facilities/facilities/?fields=id%2Ccode%2Cname%2Cofficial_name%2Cregulatory_status_name%2Cupdated%2Cfacility_type_name%2Cowner_name%2Ccounty%2Csub_county_name%2Crejected%2Cward_name%2Ckeph_level%2Ckeph_level_name%2Cconstituency_name%2Cis_complete%2Cin_complete_details%2Capproved%2Cis_approved%2Capproved_national_level&format=json"
     
 
