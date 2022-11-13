a5370f278f12cddd2f8dac3b11e07dcdef062cb2
Mori Keli <kelimbth@gmail.com>
2022-11-13 10:31:16 +0300

    added a few codes

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,15 +4,31 @@ import json
 
 def index_view(request):
     API_KEY = "yE2wzM3G6eSS5FyhXLzPLsWPihwMFc"
-    url_route  = "http://api.kmhfltest.health.go.ke/api/facilities/facilities/?fields=id%2Ccode%2Cname%2Cofficial_name%2Cregulatory_status_name%2Cupdated%2Cfacility_type_name%2Cowner_name%2Ccounty%2Csub_county_name%2Crejected%2Cward_name%2Ckeph_level%2Ckeph_level_name%2Cconstituency_name%2Cis_complete%2Cin_complete_details%2Capproved%2Cis_approved%2Capproved_national_level&format=json"
+    BASE_URL = "http://api.kmhfltest.health.go.ke/api/facilities/facilities/?fields=id%2Ccode%2Cname%2Cofficial_name%2Cregulatory_status_name%2Cupdated%2Cfacility_type_name%2Cowner_name%2Ccounty%2Csub_county_name%2Crejected%2Cward_name%2Ckeph_level%2Ckeph_level_name%2Cconstituency_name%2Cis_complete%2Cin_complete_details%2Capproved%2Cis_approved%2Capproved_national_level&format=json"
     
 
-    # REQUEST_URL = f'{BASE_URL}'
     headers = {'Authorization': f'Bearer {API_KEY}'}
-    response =  requests.get(url_route, headers=headers)
+    response =  requests.get(BASE_URL, headers=headers)
+
     data = response.json()
-    print(data)
+    
+    filter_data = {
+        'county': data["results"][0]["county"],
+        'sub_county': data["results"][0]["sub_county_name"],
+        'constituency_name': data["results"][0]["constituency_name"],
+        'ward': data["results"][0]["ward_name"],
 
-    context = {'response': data}
+    }
+
+    for i in data:
+        print(data["results"][6]["county"])
+
+    print(len(data))
+    context = {
+        'total_count': data,
+        'county': filter_data['county'],
+        'sub_county': filter_data["sub_county"],
+        'constituency': filter_data["constituency_name"],
+        'ward': filter_data["ward"]
+    }
     return render(request, 'dashboard/index.html', context)
-
